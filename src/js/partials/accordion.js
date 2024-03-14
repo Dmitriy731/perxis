77117ae4c380bf6399c74ecfff893ce9c05204b3
Dmitriy <Evgrashin.Dmitriy@yandex.ru>
2024-03-14 11:02:35 +0400

    main

--- src/js/partials/accordion.js
+++ src/js/partials/accordion.js
@@ -5,9 +5,9 @@
         const accordion = item.closest('.js-accordion');
         const content = accordion.querySelector('.js-content-accordion')
         const height = accordion.querySelector('.js-height-accordion')
+        const heightContent = height.offsetHeight;
 
-        item.addEventListener('click', function() {
-            const heightContent = height.offsetHeight;
+        function accordionFunc() {
 
             if(content.offsetHeight === 0) {
                 accordion.classList.add('open')
@@ -15,6 +15,24 @@
             } else {
                 accordion.classList.remove('open')
                 content.style.height = 0
+            }
+        }
+
+        if(window.innerWidth >= 768) {
+            content.style.height = `${heightContent + 'px'}`
+            item.removeEventListener('click', accordionFunc, false);
+        } else {
+            content.style.height = 0
+            item.addEventListener('click', accordionFunc, false)
+        }
+
+        window.addEventListener('resize', function() {
+            if(window.innerWidth >= 768) {
+                content.style.height = `${heightContent + 'px'}`
+                item.removeEventListener('click', accordionFunc, false);
+            } else {
+                content.style.height = 0
+                item.addEventListener('click', accordionFunc, false)
             }
         })
     })
