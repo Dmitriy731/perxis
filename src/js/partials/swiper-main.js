8cbcac40eede9dd25b5c565c93b09a5258ffa5c8
Dmitriy <Evgrashin.Dmitriy@yandex.ru>
2024-03-18 17:42:54 +0400

    main

--- src/js/partials/swiper-main.js
+++ src/js/partials/swiper-main.js
@@ -1,44 +1,47 @@
 (function () {
-    let isInit;
-    var swiper;
+    const slider = document.querySelectorAll('.js-slider');
 
-    function initSwiper() {
-        swiper = new Swiper('.js-stock-slider', {
-            speed: 500,
-            loop: true,
-            spaceBetween: 18,
-            slidesPerView: 1,
+    slider.forEach(item => {
+        let isInit;
+        var swiper;
+        const pagination = item.querySelector('.b-swiper-pagination')
 
-            pagination: {
-                el: '.js-stock-slider .b-swiper-pagination',
-                clickable: true,
-            },
+        function initSwiper() {
+            swiper = new Swiper(item, {
+                speed: 500,
+                loop: true,
+                spaceBetween: 18,
+                slidesPerView: 1,
 
-            breakpoints: {
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
+                pagination: {
+                    el: pagination,
+                    clickable: true,
                 },
-            }
-        });
-        isInit = true;
-    }
 
-    if (window.innerWidth < 1024) {
-        initSwiper();
-    }
-
-    window.addEventListener('resize', function () {
-        if (window.innerWidth >= 1024 && isInit) {
-            swiper.destroy();
-            isInit = false;
-        }
-
-        if (window.innerWidth < 1024 && !isInit) {
-            initSwiper();
+                breakpoints: {
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                }
+            });
             isInit = true;
         }
+
+        if (window.innerWidth < 1024) {
+            initSwiper();
+        }
+
+        window.addEventListener('resize', function () {
+            if (window.innerWidth >= 1024 && isInit) {
+                swiper.destroy();
+                isInit = false;
+            }
+
+            if (window.innerWidth < 1024 && !isInit) {
+                initSwiper();
+                isInit = true;
+            }
+        })
     })
-
-
 })();
